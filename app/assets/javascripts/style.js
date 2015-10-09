92095ef4893ee49243ef910083b19adfb5971228
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-10-09 18:07:16 -0400

    adding in js functions and listings nav

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -1,14 +1,60 @@
 $(document).ready(function(){
 
 	$("#gondolas-listings-show-pictures").hide();
+	$(".listings-show-lists").hide();
 
-	$("#gondolas").on("click", function(){
+
+
+
+	SetNames('gondolas');
+	SetNames('mannequins');
+	SetNames('pharmacy_displays');
+	SetNames('security_mirrors');
+	SetNames('shopping_carts_and_baskets');
+	SetNames('showcases');
+	SetNames('slatwall_and_accessories');
+	SetNames('tags_and_guns');
+	SetNames('wire_baskets');
+	SetNames('racks');
+	SetNames('hangers');
+	SetNames('gridwall');
+
+
+	SetLiSelector(1, 'gondolas');
+	SetLiSelector(2, 'mannequins');
+	SetLiSelector(3, 'pharmacy_displays');
+	SetLiSelector(4, 'security_mirrors');
+	SetLiSelector(5, 'shopping_carts_and_baskets');
+	SetLiSelector(6, 'showcases');
+	SetLiSelector(7, 'slatwall_and_accessories');
+	SetLiSelector(8, 'tags_and_guns');
+	SetLiSelector(9, 'wire_baskets');
+	SetLiSelector(10, 'racks');
+	SetLiSelector(11, 'hangers');
+	SetLiSelector(12, 'gridwall');
+
+
+
+function SetNames(name) {
+	$("#" + name).on("click", function(){
 		event.preventDefault();
-		$("#gondolas-listings-show-pictures").show();
+		$("#" + name + "-listings-show-pictures").show();
+		$(".products").fadeOut();
+	})
+
+} 
+
+function SetLiSelector(li_id, name) {
+	$("#listings-nav li:nth-child(" + li_id + ")").on("click", function(){
+		$(".listings-show-lists").fadeOut();
+		$("#" + name + "-listings-show-pictures").show();
 		$(".products").fadeOut();
 
-
 	})
+
+
+}
+
 
 
 
